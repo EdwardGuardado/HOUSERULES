e11b71560c9ed1c6512703bbf92a5d2a08276b38
milanes1 <arturo@umail.ucsb.edu>
2017-02-24 03:53:44 -0800

    Update MyEventsActivity.java

--- app/src/main/java/com/edward/cs48/houserules/EventActivities/MyEventsActivity.java
+++ app/src/main/java/com/edward/cs48/houserules/EventActivities/MyEventsActivity.java
@@ -1,20 +1,62 @@
 package com.edward.cs48.houserules.EventActivities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MyEventsActivity extends AppCompatActivity {
+public class MyEventsActivity extends ListActivity{ //AppCompatActivity {
 
     private static final String TAG = "MyEventsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        String[] FRUITS = new String[] { "Chess tournament R1","Chess tournament R2",
+                "Chess tournament R3","Chess tournament QuarterFinals","Chess tournament SemiFinals","Chess tournament Championship","DParty","The Rob Concert"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                FRUITS);
+        setListAdapter(adapter);
     }
 
+
     @Override
-    protected void onResume() {
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onResume(){
         super.onResume();
     }
+
+
+
+
+
+
 }
