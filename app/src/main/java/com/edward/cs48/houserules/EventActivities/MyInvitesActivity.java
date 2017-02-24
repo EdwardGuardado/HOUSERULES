c934346ee2593b7641e16efc6030d62818433318
milanes1 <arturo@umail.ucsb.edu>
2017-02-24 03:55:02 -0800

    Update MyInvitesActivity.java

--- app/src/main/java/com/edward/cs48/houserules/EventActivities/MyInvitesActivity.java
+++ app/src/main/java/com/edward/cs48/houserules/EventActivities/MyInvitesActivity.java
@@ -1,30 +1,71 @@
 package com.edward.cs48.houserules.EventActivities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MyInvitesActivity extends AppCompatActivity {
+import com.edward.cs48.houserules.R;
+
+
+public class MyInvitesActivity extends ListActivity {// AppCompatActivity{
 
     private static final String TAG = "MyInvitesActivity";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+
+        String[] events = new String[] { "Greek party", "love in the air", "Banana fun",
+                "Roast Rob", "Coconut sunday", "chess tournament", "Soccer game", "eddie the catcher",
+                "dance of", "food contenst", "Daniel's sun glasses","Gio's Leg Day", "adopt a pet"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                events);
+        setListAdapter(adapter);
     }
 
+
     @Override
-    protected void onResume() {
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onResume(){
         super.onResume();
     }
 
-    @Override
+
+
+
+
+
+}
+
+   /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
@@ -34,4 +75,4 @@ public class MyInvitesActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-}
+}*/
