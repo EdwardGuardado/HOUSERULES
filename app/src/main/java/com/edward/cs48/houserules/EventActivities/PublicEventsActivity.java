be129531d166e8a94a0d10660f9533fd60e97879
milanes1 <arturo.e.m.n@gmail.com>
2017-03-13 19:17:13 -0700

    public events added

---
+++ app/src/main/java/com/edward/cs48/houserules/EventActivities/PublicEventsActivity.java
@@ -0,0 +1,106 @@
+package com.edward.cs48.houserules.EventActivities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.edward.cs48.houserules.HouseRulesEvent.houseRulesEvent;
+import com.edward.cs48.houserules.HouseRulesUser.houseRulesUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class PublicEventsActivity extends ListActivity{ //AppCompatActivity {
+
+    private static final String TAG = "PublicEventsActivity";
+    private ArrayList<String> events = new ArrayList<String>();
+
+    private FirebaseAuth auth;
+    private FirebaseDatabase userDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference userReference;
+    private houseRulesUser ourUser;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        auth = FirebaseAuth.getInstance();
+        userReference = userDatabase.getReference("user/userdatabase/"+ auth.getCurrentUser().getUid() + "/");
+        super.onCreate(savedInstanceState);
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                ourUser = dataSnapshot.getValue(houseRulesUser.class);
+                setup();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("Failed to load user", "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        userReference.addValueEventListener(userListener);
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    public void setup(){
+        for (houseRulesEvent event : ourUser.getHostEventList()) {
+            events.add(event.getName());
+        }
+        for (houseRulesEvent event : ourUser.getAttendEventList()) {
+            events.add(event.getName());
+        }
+        String[] newEvents = events.toArray(new String[events.size()]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,newEvents);
+        setListAdapter(adapter);
+    }
+
+
+}
